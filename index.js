0197f6c0599e8394df7b8a9b601fcce9733e4db9
Suyash Dhakal <suyash.dhakal@icloud.com>
2025-06-10 09:11:51 +0545

    feat: add MongoDB connection setup

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import { connectDB } from './db/connectDB.js';
+import dotenv from 'dotenv';
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +11,7 @@ app.use(express.json()); // Middleware to parse JSON bodies
 app.get('/', (req, res) => res.send('Hello, Node.js!'));
 
 app.listen(PORT, () => {
+    connectDB();
   console.log(`Server is running on http://localhost:${PORT}`);
 }
 );
